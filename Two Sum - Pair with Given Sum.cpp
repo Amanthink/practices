b19a7aa6c951c647b4fbf9efbd3e380b580fe2ad
Aman chahar <colamanit@gmail.com>
2024-12-26 21:42:20 +0530

    Create Two Sum - Pair with Given Sum.cpp

---
+++ Two Sum - Pair with Given Sum.cpp
@@ -0,0 +1,64 @@
+Given an array arr[] of positive integers and another integer target. Determine if there exists two distinct indices such that the sum of there elements is equals to target.
+
+Examples:
+
+Input: arr[] = [1, 4, 45, 6, 10, 8], target = 16
+Output: true
+Explanation: arr[3] + arr[4] = 6 + 10 = 16.
+Input: arr[] = [1, 2, 4, 3, 6], target = 11
+Output: false
+Explanation: None of the pair makes a sum of 11.
+Input: arr[] = [11], target = 11
+Output: false
+Explanation: No pair is possible as only one element is present in arr[].
+Constraints:
+1 ≤ arr.size ≤ 105
+1 ≤ arr[i] ≤ 105
+1 ≤ target ≤ 2*105
+
+
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Solution {
+  public:
+    bool twoSum(vector<int>& arr, int target) {
+unordered_map<int, int> numMap; // To store array values and their indices
+        for (int i = 0; i < arr.size(); i++) {
+            int complement = target - arr[i];
+            if (numMap.find(complement) != numMap.end()) {
+                return true; // Pair found
+            }
+            numMap[arr[i]] = i; // Add current element to the map
+        }
+        return false; // No pair found
+    }
+    };
