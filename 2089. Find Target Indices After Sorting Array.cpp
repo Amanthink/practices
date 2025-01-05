4f96ff81e581c91d1c62a154e6120f49af35d7b4
Aman chahar <colamanit@gmail.com>
2025-01-05 23:58:38 +0530

    Update 2089. Find Target Indices After Sorting Array.cpp

--- 2089. Find Target Indices After Sorting Array.cpp
+++ 2089. Find Target Indices After Sorting Array.cpp
@@ -1,3 +1,5 @@
+
+//by using sort function
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
@@ -6,6 +8,36 @@ public:
         sort(nums.begin(), nums.end());
         
         // Find target indices
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target)
+                ans.push_back(i);
+        }
+        return ans;
+    }
+};
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+//without using sort function
+    class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        vector<int> ans;
+
+      
+        for (int i = 0; i < nums.size() - 1; i++) {
+            int mini = i; 
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[j] < nums[mini]) {
+                    mini = j; 
+                }
+            }
+            if (mini != i) {
+                int temp = nums[mini];
+                nums[mini] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+       
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == target)
                 ans.push_back(i);
