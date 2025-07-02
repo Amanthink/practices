b81aaca616ae528bcae4f847f5b38aa249686cdb
Aman chahar <colamanit@gmail.com>
2025-07-02 23:30:19 +0530

    Create 137. Single Number II.cpp

---
+++ 137. Single Number II.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int, int> freq;
+        for (int num : nums) freq[num]++;
+        for (auto& [num, count] : freq) {
+            if (count == 1) return num;
+        }
+        return -1; // If none found
+    }
+};
