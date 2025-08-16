1b5e76c8476374c0076b28da0e2a7c7f9c602af6
Aman chahar <colamanit@gmail.com>
2025-08-16 14:42:32 +0530

    Create 1305. All Elements in Two Binary Search Trees.cpp

---
+++ 1305. All Elements in Two Binary Search Trees.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& result) {
+        if (!root) return;
+        inorder(root->left, result);
+        result.push_back(root->val);
+        inorder(root->right, result);
+    }
+    vector<int> getAllElements(TreeNode* root1, TreeNode* root2) {
+        vector<int> ans;
+        inorder(root1,ans);
+        inorder(root2,ans);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
